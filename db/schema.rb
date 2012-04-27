63a7151f8e9592fa3a2b5f84f4574f9ffcf4e52c
ADigitalNative <adigitalnative@gmail.com>
2012-04-27 14:29:29 -0400

    Assignments exist, Sorcery/users still buggy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120427180619) do
+ActiveRecord::Schema.define(:version => 20120427182800) do
+
+  create_table "assignments", :force => true do |t|
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "git_hub"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username",         :null => false
